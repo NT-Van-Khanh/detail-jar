5eee21e93d1654858d2c5783f775d80982b70e65
NT-Van-Khanh <ntvk137@gmail.com>
2024-11-23 23:16:35 +0700

    build file jar for project

--- retail/src/main/java/pthttm/retail/config/FirebaseConfig.java
+++ retail/src/main/java/pthttm/retail/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
@@ -15,13 +16,27 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.credentials-path}")
+    private String firebaseCredentialsPath;
+
+    @Value("${firebase.storage.bucket-name}")
+    private String firebaseStorageBucketName;
+//          FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/firebase-key.json");
+//          InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase/firebase-key.json");
+
     @PostConstruct
     public void initialize(){
         try{
-//            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/firebase-key.json");
-            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase/firebase-key.json");
+            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream(firebaseCredentialsPath);
+            if (serviceAccount == null) {
+                throw new RuntimeException("Firebase service account key file not found in classpath");
+            }
             FirebaseOptions options = new FirebaseOptions.Builder().
-                    setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+                    setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setStorageBucket(firebaseStorageBucketName)
+                    .build();
+
+            // Chỉ khởi tạo FirebaseApp nếu chưa có
             if(FirebaseApp.getApps().isEmpty()){
                 FirebaseApp.initializeApp(options);
             }
